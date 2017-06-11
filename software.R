87a07b7491ffd47b29c94ab939f6a808165e6d34
Andy Krause <andy.krause@unimelb.edu.au>
2017-06-11 09:59:58 -0700

    Initial commit of Chp 3 code (software)

---
+++ software.R
@@ -0,0 +1,29 @@
+##########################################################################################
+#                                                                                        #            
+#  Necessary R Packages for Real Estate Analysis in the Information Age                  #   
+#                                                                                        #                  
+##########################################################################################
+
+## Install the necessary libraries
+
+  install.packages('tidyverse')
+  install.packages('sf')
+  install.packages('sp')
+  install.packages('maptools')
+  install.packages('rgeos')
+  install.packages('spgwr')
+  install.packages('spdep')
+  install.packages('geosphere')
+  install.packages('OpenStreetMap')
+  install.packages('gstat')
+  install.packages('RODBC')
+  install.packages('RSQLite')
+  install.packages('BMA')
+  install.packages('MASS')
+  install.packages('car')
+  install.packages('lmtest')
+  install.packages('plm')
+  install.packages('scales')
+  
+##########################################################################################
+##########################################################################################
